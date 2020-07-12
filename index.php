9afd5d59592702ac87cc211b00d9f7d1ee14d5ca
mambax7 <mambax7@gmail.com>
2020-07-11 22:46:43 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,6 @@
+<?
+include("../../mainfile.php");
+include("../../header.php");
+include("mp3player.html");
+include("header.php");
+include("../../footer.php");
---
+++ xoops_version.php
@@ -0,0 +1,31 @@
+<?php
+
+/*********************************************************/
+/* Sahra Ýnternet ve Biliþim Hizmetleri Xoops Destek Bölümü                      */
+/* Sahra Tarafýndan Yapýlmýþtýr                                         */
+/* Not:Ücretsiz Bir Modüldür.Ýstediðiniz Gibi Geliþtirebilirsiniz                                    */
+/* http://www.xoopsum.com                            */
+/*                            */
+/*                          */
+/*********************************************************/
+
+/**
+*
+*  Muzik-Calar Modülü
+*  Xoopsum.Com | Sahra Ýnternet Hizmetleri Genel Yönetici Sahra Tarafýndan Oluþturulmuþtur
+*  
+*
+*/
+
+$modversion['name']         = "Muzik-Calar";
+$modversion['dirname']      = "Muzik-Calar";
+$modversion['hasMain']      = "1";
+$modversion['description']  = "XOOPS Muzik-Calar";
+$modversion['version']      = "1.1";
+$modversion['author']       = "Xoopsum.Com|Sahra Ýnternet Hizmetleri";
+$modversion['credits']      = "Sahra";
+$modversion['license']      = "GPL";
+$modversion['official']     = "No";
+$modversion['image']        = "images/logo.gif";
+
+?>
